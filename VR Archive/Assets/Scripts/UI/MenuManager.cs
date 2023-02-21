f2b3d35a47dd0d8265bdffc18c023618c7ccfea2
John Pham <54965720+johnpham99@users.noreply.github.com>
2023-02-20 19:29:36 -0500

    Add Main Menu Scene

---
+++ VR Archive/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Panel currentPanel = null;
+
+    private List<Panel> panelHistory = new List<Panel>();
+
+    private void Start()
+    {
+        SetupPanels();
+    }
+
+    private void SetupPanels()
+    {
+        Panel[] panels = GetComponentsInChildren<Panel>();
+        foreach (Panel panel in panels)
+            panel.Setup(this);
+
+        currentPanel.Show();
+    }
+}
